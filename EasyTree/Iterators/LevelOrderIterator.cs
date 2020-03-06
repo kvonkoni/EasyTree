2b2e2f9c715580be4cd7c53a02ebfc29ef574b3b
Kier von Konigslow <kvonkonigslow@gmail.com>
2020-03-06 14:50:20 -0500

    Changed scope of the queue

--- EasyTree/Iterators/LevelOrderIterator.cs
+++ EasyTree/Iterators/LevelOrderIterator.cs
@@ -6,46 +6,44 @@ namespace EasyTree.Iterators
 {
     public class LevelOrderIterator : IteratorBase
     {
-        private readonly Queue<Node> _queue;
-
         public LevelOrderIterator(Node node) : base(node)
         {
-            _queue = new Queue<Node>();
             LevelOrder(_node);
         }
 
         public LevelOrderIterator(Node node, PerformFunction function) : base(node)
         {
-            _queue = new Queue<Node>();
             LevelOrder(_node, function);
         }
 
         private void LevelOrder(Node node)
         {
+            var queue = new Queue<Node>();
             _nodelist.Add(node);
-            _queue.Enqueue(node);
-            while (_queue.Count > 0)
+            queue.Enqueue(node);
+            while (queue.Count > 0)
             {
-                foreach(Node currentNode in _queue.Dequeue().Children)
+                foreach(Node currentNode in queue.Dequeue().Children)
                 {
                     _nodelist.Add(currentNode);
-                    _queue.Enqueue(currentNode);
+                    queue.Enqueue(currentNode);
                 }
             }
         }
 
         private void LevelOrder(Node node, PerformFunction function)
         {
+            var queue = new Queue<Node>();
             _nodelist.Add(node);
             function(node);
-            _queue.Enqueue(node);
-            while (_queue.Count > 0)
+            queue.Enqueue(node);
+            while (queue.Count > 0)
             {
-                foreach (Node currentNode in _queue.Dequeue().Children)
+                foreach (Node currentNode in queue.Dequeue().Children)
                 {
                     _nodelist.Add(currentNode);
                     function(node);
-                    _queue.Enqueue(currentNode);
+                    queue.Enqueue(currentNode);
                 }
             }
         }
