0dcfb2b0ada8697bca9e5efa6f195bf9cb6e8b0d
Kier von Konigslow <kvonkonigslow@gmail.com>
2020-12-08 11:58:38 -0500

    Added exception and unittest for addchild when there is a loop

--- EasyTree.XUnitTests/NodeUnitTests.cs
+++ EasyTree.XUnitTests/NodeUnitTests.cs
@@ -6,49 +6,6 @@ namespace EasyTree.XUnitTests
     public class NodeUnitTests
     {
         [Fact]
-        public void Test_AddParent_ValidChildren_RootLeafDescendantPathPropertiesCorrect()
-        {
-            var root = new Node();
-            var childA = new Node();
-            var childB = new Node();
-            var childC = new Node();
-
-            childA.AddParent(root);
-            childB.AddParent(childA);
-            childC.AddParent(childB);
-
-            // Making sure only 'root' is a root
-            Assert.True(root.IsRoot);
-            Assert.False(childA.IsRoot);
-            Assert.False(childB.IsRoot);
-            Assert.False(childC.IsRoot);
-
-            // Making sure each node's root is 'root'
-            Assert.Equal(root, root.Root);
-            Assert.Equal(root, childA.Root);
-            Assert.Equal(root, childB.Root);
-            Assert.Equal(root, childC.Root);
-
-            // Making sure only 'childC' is a leaf
-            Assert.False(root.IsLeaf);
-            Assert.False(childA.IsLeaf);
-            Assert.False(childB.IsLeaf);
-            Assert.True(childC.IsLeaf);
-
-            // Making sure the lists of descendants are correct
-            Assert.Equal(new HashSet<Node>() { childA, childB, childC }, root.Descendants);
-            Assert.Equal(new HashSet<Node>() { childB, childC }, childA.Descendants);
-            Assert.Equal(new HashSet<Node>() { childC }, childB.Descendants);
-            Assert.Empty(childC.Descendants);
-
-            // Making sure all paths are correct
-            Assert.Equal(new List<Node>() { root }, root.Path);
-            Assert.Equal(new List<Node>() { root, childA }, childA.Path);
-            Assert.Equal(new List<Node>() { root, childA, childB }, childB.Path);
-            Assert.Equal(new List<Node>() { root, childA, childB, childC }, childC.Path);
-        }
-
-        [Fact]
         public void Test_AddChild_ValidChildren_RootLeafDescendantPathPropertiesCorrect()
         {
             var root = new Node();
@@ -89,6 +46,17 @@ namespace EasyTree.XUnitTests
             Assert.Equal(new List<Node>() { root, childA }, childA.Path);
             Assert.Equal(new List<Node>() { root, childA, childB }, childB.Path);
             Assert.Equal(new List<Node>() { root, childA, childB, childC }, childC.Path);
+        }
+
+        [Fact]
+        public void Test_AddChild_CircularChildren_TreeStructureExceptionThrown()
+        {
+            var nodeA = new Node();
+            var nodeB = new Node();
+
+            nodeA.AddChild(nodeB);
+
+            Assert.Throws<TreeStructureException>(() => nodeB.AddChild(nodeA));
         }
 
         [Fact]
---
+++ EasyTree/TreeStructureException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EasyTree
+{
+    public class TreeStructureException : EasyTreeException
+    {
+        public TreeStructureException(string message) : base(message)
+        {
+        }
+    }
+}
