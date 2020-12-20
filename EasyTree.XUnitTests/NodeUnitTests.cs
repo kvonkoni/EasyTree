ea6f8e68718233b7d918ab39657e3b1cc2ff99aa
Kier von Konigslow <kvonkonigslow@gmail.com>
2020-12-20 15:30:57 -0500

    Created test for the merging of two trees

--- EasyTree.XUnitTests/NodeUnitTests.cs
+++ EasyTree.XUnitTests/NodeUnitTests.cs
@@ -49,6 +49,105 @@ namespace EasyTree.XUnitTests
         }
 
         [Fact]
+        public void Test_CombineTwoTrees_ValidSubTrees_RootLeafDescendantPathPropertiesCorrect()
+        {
+            var tree1 = new Node();
+            var child1A = new Node();
+            var child1B = new Node();
+            var child1C = new Node();
+
+            tree1.AddChild(child1A);
+            child1A.AddChild(child1B);
+            child1B.AddChild(child1C);
+
+            var tree2 = new Node();
+            var child2A = new Node();
+            var child2B = new Node();
+            var child2AchildA = new Node();
+            var child2AchildB = new Node();
+
+            tree2.AddChild(child2A);
+            tree2.AddChild(child2B);
+            child2A.AddChild(child2AchildA);
+            child2A.AddChild(child2AchildB);
+
+            var root = new Node();
+            root.AddChild(tree1);
+            root.AddChild(tree2);
+
+            // Making sure only 'root' is a root
+            Assert.True(root.IsRoot);
+
+            Assert.False(tree1.IsRoot);
+            Assert.False(child1A.IsRoot);
+            Assert.False(child1B.IsRoot);
+            Assert.False(child1C.IsRoot);
+
+            Assert.False(tree2.IsRoot);
+            Assert.False(child2A.IsRoot);
+            Assert.False(child2B.IsRoot);
+            Assert.False(child2AchildA.IsRoot);
+            Assert.False(child2AchildB.IsRoot);
+
+            // Making sure each node's root is 'root'
+            Assert.Equal(root, root.Root);
+            
+            Assert.Equal(root, tree1.Root);
+            Assert.Equal(root, child1A.Root);
+            Assert.Equal(root, child1B.Root);
+            Assert.Equal(root, child1C.Root);
+
+            Assert.Equal(root, tree2.Root);
+            Assert.Equal(root, child2A.Root);
+            Assert.Equal(root, child2B.Root);
+            Assert.Equal(root, child2AchildA.Root);
+            Assert.Equal(root, child2AchildB.Root);
+
+            // Making sure only 'child1C', 'child2B',
+            // 'child2AchildA', 'child2AchildB' are leaves
+            Assert.False(root.IsLeaf);
+            
+            Assert.False(tree1.IsLeaf);
+            Assert.False(child1A.IsLeaf);
+            Assert.False(child1B.IsLeaf);
+            Assert.True(child1C.IsLeaf);
+
+            Assert.False(tree2.IsLeaf);
+            Assert.False(child2A.IsLeaf);
+            Assert.True(child2B.IsLeaf);
+            Assert.True(child2AchildA.IsLeaf);
+            Assert.True(child2AchildB.IsLeaf);
+
+            // Making sure each node's set of descendants is correct
+            Assert.Equal(new HashSet<Node> { tree1, child1A, child1B, child1C, tree2, child2A, child2B, child2AchildA, child2AchildB }, root.Descendants);
+            
+            Assert.Equal(new HashSet<Node> { child1A, child1B, child1C }, tree1.Descendants);
+            Assert.Equal(new HashSet<Node> { child1B, child1C }, child1A.Descendants);
+            Assert.Equal(new HashSet<Node> { child1C }, child1B.Descendants);
+            Assert.Empty(child1C.Descendants);
+
+            Assert.Equal(new HashSet<Node> { child2A, child2B, child2AchildA, child2AchildB }, tree2.Descendants);
+            Assert.Equal(new HashSet<Node> { child2AchildA, child2AchildB }, child2A.Descendants);
+            Assert.Empty(child2B.Descendants);
+            Assert.Empty(child2AchildA.Descendants);
+            Assert.Empty(child2AchildB.Descendants);
+
+            // Making sure each node's path list is correct
+            Assert.Equal(new List<Node> { root }, root.Path);
+
+            Assert.Equal(new List<Node> { root, tree1 }, tree1.Path);
+            Assert.Equal(new List<Node> { root, tree1, child1A }, child1A.Path);
+            Assert.Equal(new List<Node> { root, tree1, child1A, child1B }, child1B.Path);
+            Assert.Equal(new List<Node> { root, tree1, child1A, child1B, child1C }, child1C.Path);
+
+            Assert.Equal(new List<Node> { root, tree2 }, tree2.Path);
+            Assert.Equal(new List<Node> { root, tree2, child2A }, child2A.Path);
+            Assert.Equal(new List<Node> { root, tree2, child2B }, child2B.Path);
+            Assert.Equal(new List<Node> { root, tree2, child2A, child2AchildA }, child2AchildA.Path);
+            Assert.Equal(new List<Node> { root, tree2, child2A, child2AchildB }, child2AchildB.Path);
+        }
+
+        [Fact]
         public void Test_AddChild_Self_TreeStructureExceptionThrown()
         {
             var node = new Node();
